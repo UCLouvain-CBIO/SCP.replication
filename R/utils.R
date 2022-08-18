449cf8c1be78487b2e8a11d6002f8a43a5cc6036
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2022-08-18 11:38:06 +0200

    Added utility functions: readSCPfromDIANN and correctIstopicCarryover

--- R/utils.R
+++ R/utils.R
@@ -368,7 +368,6 @@ ComBatv3.34 <- function (dat, batch, mod = NULL, par.prior = TRUE, prior.plots =
 
 ##' @title KNN imputation (for replication)
 ##'
-##'
 ##' @description
 ##'
 ##' The function imputes missing data using the k-nearest neighbours
@@ -527,71 +526,221 @@ pcaSCoPE2 <- function(object, scale = FALSE, center = FALSE) {
 }
 
 
-normalizeSCeptre <- function(object, i, 
-                             batchCol, 
-                             channelCol,
-                             iterThres = 1.1,
-                             name = "normAssay") {
-    stopifnot(inherits(object, "QFeatures"))
+##' @title Isotopic carryover correction for plexDIA data
+##'
+##' @description
+##' 
+##' This function performs isotopic carryover correction. Currently, 
+##' only isotopic correction for mTRAQ is available. 
+##'
+##' @param x An object that inherits from the `SummarizedExperiment`
+##'     class. 
+##'
+##' @return An object of same class as `x` with the isotopic correction
+##'     applied. 
+##' 
+##' @import OrgMassSpecR
+##' @import reticulate
+##' @import scpdata
+##' 
+##' @export
+correctIsotopicCarryover <- function (x) {
+    ## Load the python functions to compute isotopic distributions
+    pyFuns <- paste0(path.package("SCP.replication"),
+                     "/scripts/python_functions.py")
+    source_python(pyFuns)
     
-    ## Get a df from the QFeatures object
-    x <- longFormat(object[, , i], 
-                    colvars = c(batchCol, channelCol))
-    x <- x[!is.na(x$value) & x$value != 0, ]
+    ## Check arguments
+    rd <- rowData(x)
+    required <- c("Modified.Sequence", "Stripped.Sequence")
+    if (any(mis <- !required %in% colnames(rd)))
+        stop(paste(required[mis], collapse = ", "), " is/are missing in rowData(x)")
     
-    ## Apply the normalization loop
-    for (iter in 1:100) {
-        message(paste0("Iteration: ", iter))
-        value0 <- x$value
-        data.frame(x) %>% 
-            ## Normalize by batch
-            ## 1. Calculate median for each protein in each batch
-            group_by_at(.vars = c(batchCol, "rowname")) %>%
-            mutate(meds = median(value, na.rm = TRUE)) %>%
-            # 2. Calculate the factors needed for a median shift
-            group_by_at("rowname") %>%
-            mutate(meansTot = median(meds, na.rm = TRUE)) %>%
-            group_by_at(c(batchCol, "rowname")) %>%
-            mutate(factors = meds / meansTot) %>%
-            # 3. Apply factors
-            ungroup() %>%
-            mutate(value = value / factors) %>%
-            ## Normalize by channel
-            ## 1. Calculate median for each protein in each sample
-            group_by_at(.vars = c(channelCol, "rowname")) %>%
-            mutate(meds = median(value, na.rm = TRUE)) %>%
-            # 2. Calculate the factors needed for a median shift
-            group_by_at(.vars = "rowname") %>%
-            mutate(meansTot = median(meds, na.rm = TRUE)) %>%
-            group_by_at(.vars = c(channelCol, "rowname")) %>%
-            mutate(factors = meds / meansTot) %>%
-            # 3. Apply factors
-            ungroup() %>%
-            mutate(value = value / factors) ->
-            x
-        
-        current <- abs(max(x$value - value0))
-        message(paste0("Current convergence: ", current))
-        if (current <= iterThres)
-            break()
+    ## Generate the atomic composition of the unmodified precursor
+    atomsPerPrec <- t(sapply(rd$Stripped.Sequence, function(x) {
+        unlist(OrgMassSpecR::ConvertPeptide(x))
+    }))
+    
+    ## Generate the atomic composition of each modified precursor
+    ## Get the atomic composition table for each modification
+    atomsPerMod <- matrix(c(rep(12, 3), rep(1, 3), -1, 0, 3, rep(1, 3), rep(0, 3)), ncol = 5,
+                          dimnames = list(c("deamid", "ox", "carba"),
+                                          c("C", "H", "N", "O", "S")))
+    ## Get the number the modifications for each precursor
+    modsPerPrec <- t(sapply(rd$Modified.Sequence, function(x) {
+        c(deamid = str_count(x, "UniMod:7"),
+          ox = str_count(x, "UniMod:35"),
+          carba = str_count(x, "UniMod:4"))
+    }))
+    ## Compute the number of atoms for the modified precursors
+    atomsPerModPrec <- atomsPerPrec + modsPerPrec %*% atomsPerMod
+    
+    ## Generate the atomic composition of each modified and labeled precursors
+    ## Get the atomic composition table for each label
+    atomsPerLabel <- matrix(c(7, 4, 1, 2, 1, 0, 0, 0, 2, -1, 0, 1, rep(0, 3)), ncol = 5,
+                            dimnames = list(paste0("mTRAQ", c(0, 4, 8)),
+                                            c("C", "H", "N", "O", "S")))
+    ## Get the number the labels per precursor
+    labelsPerPrec <- sapply(rd$Modified.Sequence, function(x) {
+        str_count(x, "mTRAQ")
+    })
+    ## Compute the number of atoms for the modified and labeled precursors
+    atomsPerModLabPrec <- labelsPerPrec %o% atomsPerLabel ## tensor operation
+    dims <- dim(atomsPerModLabPrec)
+    dimn <- dimnames(atomsPerModLabPrec)
+    for (j in seq_len(dims[2])) { ## tensor sweep with matrix
+        atomsPerModLabPrec[, j, ] <- atomsPerModLabPrec[, j, ] + atomsPerModPrec
     }
-    ## Format the data as a matrix
-    x[, c("rowname", "primary", "value")] %>%
-        pivot_wider(id_cols = "rowname", 
-                    names_from = "primary", 
-                    values_from = "value") %>%
-        column_to_rownames("rowname") %>%
-        as.matrix ->
-        x
-    ## Insert the normalized data in the QFeatures object
-    sce <- scp[[i]][rownames(x), ]
-    assay(sce) <- x
-    object <- addAssay(object, 
-                       y = sce,
-                       name = name)
-    addAssayLink(object, 
-                 from = i,
-                 to = name,
-                 varFrom = "Accession",
-                 varTo = "Accession")
+    
+    ## Get the carry over from the atomic composition
+    carryOver <- array(dim = dims[1:2], dimnames = dimn[1:2])
+    for (j in seq_len(dims[2])) {
+        ## Compute the isotopic envelope from the atomic composition
+        isoEnv <- array(dim = c(dims[1], 6))
+        for (i in seq_len(dims[1])) {
+            isoEnv[i, ] <- iso_distr(atomsPerModLabPrec[i, j, ])[1:6]
+        }
+        rs <- rowSums(isoEnv)
+        isoEnv <- sweep(isoEnv, 1, STATS = rs, "/")
+        ## Compute the carry over from the isotopic enveloppe
+        carryOver[, j] <- rowSums(isoEnv[, 5:6]) / rowSums(isoEnv[, c(1:2, 5:6)])
+    }
+    
+    ## Get the uncorrected MS1 quantifications
+    xmat <- assay(x)
+    labels <- sub("^.*(\\d)$", "mTRAQ\\1", colnames(xmat))
+    ## The label cross-contamination design
+    design <- matrix(c(1, -1, 0, 0, 1, -1, 0, 0, 1), nrow = 3, 
+                     dimnames = list(dimn[[2]], dimn[[2]]))
+    ## Compute isotopic correction
+    xmatzero <- xmat
+    xmatzero[is.na(xmatzero)] <- 0 ## This is to ignore NAs 
+    assay(x) <- xmat + xmatzero * (carryOver %*% design)[,labels]
+    x
+}
+
+##' @title  Read DIA-NN output as a QFeatures objects for single-cell 
+##' proteomics data
+##'
+##' @description
+##' 
+##' This function takes the output tables from DIA-NN and converts them
+##' into a QFeatures object using the scp framework. 
+##'
+##' @param colData A data.frame or any object that can be coerced to a
+##'     data.frame. colData is expected to contains all the sample 
+##'     annotations. We require the table to contain a column called
+##'     `File.Name` that links to the `File.Name` in the DIA-NN report
+##'     table. If `multiplexing = "mTRAQ"`, we require a second column
+##'     called `Label` that links the label to the sample (the labels
+##'     identified by DIA-NN can be retrieved from `Modified Sequence`
+##'     column in the report table). 
+##' @param reportData A data.frame or any object that can be coerced 
+##'     to a data.frame that contains the data from the `Report.tsv`
+##'     file generated by DIA-NN. 
+##' @param extractedData A data.frame or any object that can be coerced 
+##'     to a data.frame that contains the data from the `*_ms1_extracted.tsv`
+##'     file generated by DIA-NN. This argument is optional and is 
+##'     only applicable for mulitplixed experiments 
+##' @param multiplexing A `character(1)` indicating the type of 
+##'     multiplexing used in the experiment. Provide `"none"` if the
+##'     experiment is label-free (default). Available options are:
+##'     `"mTRAQ"`. 
+##' @param ... Further arguments passed to `readSCP()`
+##'
+##' @return An instance of class QFeatures. The expression data of 
+##'     each acquisition run is stored in a separate assay as a 
+##'     SingleCellExperiment object.
+##'     
+##' @export
+##' 
+readSCPfromDIANN <- function(colData, reportData, extractedData = NULL,
+                             multiplexing = "none", # "none" or "mTRAQ"
+                             ...) {
+    diannReportCols <- c("File.Name", "Precursor.Id", "Modified.Sequence",
+                         "Ms1.Area")
+    if (!all(diannReportCols %in% colnames(reportData)))
+        stop("'reportData' is not an expected DIA-NN report table ",
+             "output. This function expects the main output file as ",
+             "described here: https://github.com/vdemichev/DiaNN#main-output-reference")
+    if (!"File.Name" %in% colnames(colData)) 
+        stop("'colData' must contain a column named 'File.Name' that provides ",
+             "a link to the 'File.Name' column in 'reportData'")
+    reportData$File.Name <- make.names(reportData$File.Name)
+    if (multiplexing == "none" && !is.null(extractedData)) 
+        stop("Providing 'extractedData' for label-free experiments ",
+             "('multiplexed == \"none\"') is not expected. Raise an ",
+             "issue if you need this feature: ",
+             "https://github.com/UCLouvain-CBIO/scp/issues/new/choose")
+    
+    args <- list(...)
+    ## Get the label used for the reportData
+    if (multiplexing == "mTRAQ") {
+        ## Extracted the mTRAQ label from the modified sequence
+        reportData$Label <- sub("^.*[Q-](\\d).*$", "\\1", reportData$Modified.Sequence)
+        reportData$Precursor.Id <- gsub("\\(mTRAQ.*?\\)", "(mTRAQ)", reportData$Precursor.Id)
+        args$sep <- "."
+        ## Make sure the colData has the Label column
+        if (!"Label" %in% colnames(colData)) 
+            stop("'colData' must contain a column named 'Label' that ",
+                 "provides the mTRAQ reagent used to label the ", 
+                 "samples and/or single cells.")
+        if (any(mis <- !colData$Label %in% reportData$Label)) {
+            stop("Some labels from 'colData$Label' were not found as",
+                 "part of the mTRAQ labels found in ",
+                 "'reportData$Modified.Sequence': ",
+                 paste0(unique(colData$Label[mis]), collapse = ", "))
+        }
+    } else if (multiplexing == "none") {
+        colData$Label <- "Ms1.Area"
+        args$sep <- ""
+    } else {
+        stop("The '", multiplexing, "' multiplexing strategy is not ",
+             "implemented. Raise an issue if you need this feature: ",
+             "https://github.com/UCLouvain-CBIO/scp/issues/new/choose")
+    }
+    ## Identify which variables are correlated with the run-specific
+    ## precursor IDs
+    nIds <- length(unique(paste0(reportData$Precursor.Id, reportData$File.Name)))
+    nLevels <- sapply(colnames(reportData), function(x) {
+        nrow(unique(reportData[, c("Precursor.Id", "File.Name", x)]))
+    })
+    idCols <- names(nLevels)[nLevels == nIds]
+    ## Transform the reportData to a wide format with respect to label
+    reportData <- pivot_wider(reportData, id_cols = all_of(idCols),
+                              names_from = "Label", 
+                              values_from = "Ms1.Area")
+    
+    ## Read using readSCP
+    out <- do.call(readSCP, c(args, list(featureData = reportData,
+                                         colData = colData,
+                                         batchCol = "File.Name",
+                                         channelCol = "Label")))
+    
+    ## Optionally, add the extractedData
+    if (!is.null(extractedData)) {
+        labs <- unique(colData$Label)
+        ## DIA-NN appends the label to the run name
+        quantCols <- grep(paste0("[", paste0(labs, collapse = ""), "]$"), 
+                          colnames(extractedData))
+        extractedData <- readSingleCellExperiment(extractedData,
+                                                  ecol = quantCols,
+                                                  fnames = "Precursor.Id")
+        ## Make sure extractedData has the sames samples as reportData
+        cnames <- unique(unlist(colnames(out)))
+        if (any(mis <- !cnames %in% colnames(extractedData)))
+            stop("Some columns present in reportData are not found in ",
+                 "extracted data", paste0(cnames[mis], collapse = ", "),
+                 "\nAre you sure the two tables were generated from ",
+                 "the same experiment?")
+        extractedData <- extractedData[, cnames]
+        ## Add the assay to the QFeatures object
+        anames <- names(out)
+        out <- addAssay(out, extractedData, name = "Ms1Extracted")
+        out <- addAssayLink(out, 
+                            from = anames, to = "Ms1Extracted",
+                            varFrom = rep("Precursor.Id", length(anames)), 
+                            varTo = "Precursor.Id")
+    }
+    out
 }
